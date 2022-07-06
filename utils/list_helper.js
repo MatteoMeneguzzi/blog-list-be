d8c28e4989336162595a61ec1748ddb1564b401f
Matteo Meneguzzi <matteo.meneguzzi@temera.it>
2022-07-06 14:44:09 +0200

    4.4: helper functions and unit tests, step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,6 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => 1;
 
+const totalLikes = (blogs) => {
+  let total = 0;
+  blogs.forEach((element) => {
+    total += element.likes;
+  });
+  return total;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
