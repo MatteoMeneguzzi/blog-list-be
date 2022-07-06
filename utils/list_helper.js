628d32a2b2a8e808543cacbc6c7a4bf89f7a9bbb
Matteo Meneguzzi <matteo.meneguzzi@temera.it>
2022-07-06 15:34:38 +0200

    4.5*: helper functions and unit tests, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -9,7 +9,32 @@ const totalLikes = (blogs) => {
   return total;
 };
 
+const favoriteBlog = (blogs) => {
+  const values = blogs.map((element) => element.likes);
+
+  let max = values[0];
+
+  // eslint-disable-next-line no-plusplus
+  for (let index = 0; index < values.length; index++) {
+    const element = values[index];
+    if (element > max) max = element;
+  }
+
+  const favorites = blogs.filter((blog) => blog.likes === max);
+
+  const returnedFavorites = favorites.map((blog) => ({
+    title: blog.title,
+    author: blog.author,
+    likes: blog.likes,
+  }));
+
+  console.log(returnedFavorites[0]);
+
+  return returnedFavorites[0];
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
