5ffa276c0233bb4e25c55be9ad8ec88a64c25171
Matteo Meneguzzi <matteo.meneguzzi@temera.it>
2022-07-06 16:33:20 +0200

    4.6*: helper functions and unit tests, step4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -28,13 +28,44 @@ const favoriteBlog = (blogs) => {
     likes: blog.likes,
   }));
 
-  console.log(returnedFavorites[0]);
-
   return returnedFavorites[0];
+};
+
+const mostBlogs = (blogs) => {
+  const values = blogs.map((element) => element.author);
+
+  const counts = {};
+  let compare = 0;
+  let mostFrequent;
+
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0, len = values.length; i < len; i++) {
+    const author = values[i];
+
+    if (counts[author] === undefined) {
+      counts[author] = 1;
+    } else {
+      counts[author] += 1;
+    }
+    if (counts[author] > compare) {
+      compare = counts[author];
+      mostFrequent = values[i];
+    }
+  }
+
+  const mostBlogsAuthor = {};
+
+  mostBlogsAuthor.author = mostFrequent;
+  mostBlogsAuthor.blogs = compare;
+
+  console.log(mostBlogsAuthor);
+
+  return mostBlogsAuthor.author === undefined ? undefined : mostBlogsAuthor;
 };
 
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 };
